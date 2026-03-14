d6bf7991867d51fb82222357e742a5bb2026585d
MONISHV <monishv@localhost.localdomain>
2026-03-14 06:51:27 +0100

    COMPARING FILES

---
+++ sender.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+
+struct message
+{
+    long type;
+    char text[100];
+};
+
+int main()
+{
+    key_t key = ftok(".", 'a');
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+
+    struct message msg;
+    msg.type = 1;
+
+    strcpy(msg.text, "monish");
+
+    msgsnd(msgid, &msg, sizeof(msg.text), 0);
+
+    return 0;
+}
